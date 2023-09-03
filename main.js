518e643c2dd651da2e428558f635e10880aab6ae
Jay <remmersjames@yahoo.com>
2023-09-03 02:46:03 -0400

    Deleting out unnessesary commented-out code
    I had a lot of code commented out trying different ways for my getWeather function to work, I wanted the attempts in my initial commit history to maybe learn from, but this is to clean up my code

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// import "./style.css"
-
-// I had to comment out the css import because it was causing a console error and I could not see my object populating in the console until the error was taken care of
-
 // import needs the js signifier, even though vscode wants to drop it
 import { getWeather } from "./weather.js"
 
